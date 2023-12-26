a8ef993eae9f102e867f8af9a5cdc19d8e87a4fb
zentaker <apalomino12@icloud.com>
2023-12-26 08:33:25 -0500

    commit

--- src/operators/time/throttletime.ts
+++ src/operators/time/throttletime.ts
@@ -11,7 +11,31 @@ salida -     --(a)----------(b)--------(c)---------(c)------------|------>
 
 */
 
-//el ultimo valor emitido en un intervalo de tiempo 
+const click$ = fromEvent(document, 'click');
+
+click$.pipe(
+    throttleTime(3000)
+).subscribe(console.log)
+
+
+//ejemplo 2
+
+//ejemplo2
+const input = document.createElement('input');
+document.querySelector('body').append(input);
+
+const input$ = fromEvent(input, 'keyup');
+
+input$.pipe(
+    throttleTime(1500, asyncScheduler, {
+        leading: false,
+        trailing: true
+    }),
+    pluck('target','value'),
+    distinctUntilChanged()
+).subscribe(console.log);
+
+
 
 
 
